3b056dc89b344b0197ed12bdfa66c2bb3a7d5d99
pdavis <paddax@gmail.com>
2016-11-14 09:51:54 +0000

    Tidied up collision detection (still may need to lift the collision regions into physics)

---
+++ app/src/main/java/com/powdermonkey/flappytots/game/BeePhysics.java
@@ -0,0 +1,40 @@
+package com.powdermonkey.flappytots.game;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import com.powdermonkey.flappytots.AbstractPhysics;
+
+import javax.vecmath.Point2f;
+
+/**
+ * Created by pdavis on 14/11/2016.
+ */
+
+public class BeePhysics extends AbstractPhysics {
+
+    @Override
+    public void update(long ts) {
+
+    }
+
+    @Override
+    public int getFrame() {
+        return 0;
+    }
+
+    @Override
+    public Point2f getSize() {
+        return null;
+    }
+
+    @Override
+    public Point2f getOffset() {
+        return null;
+    }
+
+    @Override
+    public void draw(Canvas canvas, Paint paint) {
+
+    }
+}
